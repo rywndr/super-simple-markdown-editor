521c887dd192067a251fc9524e148d9e8a438745
sudodot <haikhalroy@gmail.com>
2025-12-12 14:14:07 +0700

    batman

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from '@tanstack/react-router'
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to="/">SSME</Link>
+        </h1>
+      </header>
+    </>
+  )
+}
